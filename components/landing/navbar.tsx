4a4ad48692240aebd73a112e82791aad0e459787
sohaib440 <sohaibmushtaq5@gmail.com>
2026-07-31 00:01:40 +0500

    Latetst Code

--- components/landing/navbar.tsx
+++ components/landing/navbar.tsx
@@ -201,11 +201,11 @@ export function Navbar() {
             scrolled ? "h-16 md:h-[4.25rem] xl:h-[4.5rem]" : "h-[4.25rem] md:h-[4.5rem] xl:h-20"
           )}
         >
-          {/* <SiteLogo
+           <SiteLogo
             priority
             className="min-w-0 max-w-[min(58vw,12rem)] shrink sm:max-w-[14rem] md:max-w-[16rem] xl:max-w-[18rem]"
             imageClassName="h-8 w-auto max-w-full sm:h-9 md:h-10 xl:h-12"
-          /> */}
+          /> 
 
           <nav
             className="hidden min-w-0 flex-1 items-center justify-center gap-0.5 px-1 xl:flex 2xl:gap-1"
