9acf46489a5bad28290fcd6765ac74bb40d8d3a8
sohaib440 <sohaibmushtaq5@gmail.com>
2026-07-31 14:14:23 +0500

    Latest Code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, Playfair_Display } from "next/font/google";
+import Script from "next/script";
 
 import { ClearStaleServiceWorker } from "@/components/dev/clear-stale-sw";
 import { SiteFooter } from "@/components/landing/site-footer";
@@ -53,6 +54,22 @@ export default function RootLayout({
   return (
     <html lang="en" className={cn("h-full antialiased", inter.variable, playfair.variable)}>
       <body className="flex min-h-full w-full flex-col overflow-x-clip font-sans" suppressHydrationWarning>
+        <Script id="google-tag-manager" strategy="beforeInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MQ5RN57R');`}
+        </Script>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MQ5RN57R"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+            title="Google Tag Manager"
+          />
+        </noscript>
         <ClearStaleServiceWorker />
         <MotionInit />
         <div className="flex w-full flex-1 flex-col">{children}</div>
