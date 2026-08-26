93df332f91fbad06eca82b54aa6d691e19e541e8
sohaib440 <sohaibmushtaq5@gmail.com>
2026-08-26 12:54:11 +0500

    Latest Code

--- app/layout.tsx
+++ app/layout.tsx
@@ -76,7 +76,7 @@ export default function RootLayout({
   return (
     <html lang="en" className={cn("h-full antialiased", inter.variable, playfair.variable)}>
       <head>
-        <Script id="google-tag-manager" strategy="beforeInteractive">
+        <Script id="google-tag-manager" strategy="afterInteractive">
           {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
